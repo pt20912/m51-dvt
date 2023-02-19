279779d1b628794a17607c56a8fcfcf481184890
pt20912 <122719561+pt20912@users.noreply.github.com>
2023-02-19 13:21:42 +0700

    Update omni_m51.mk

--- omni_m51.mk
+++ omni_m51.mk
@@ -1,11 +1,11 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from m20lte device
+# Inherit from m51 device
 $(call inherit-product, device/samsung/m51/device.mk)
 PRODUCT_COPY_FILES += device/samsung/m51/prebuilt/kernel:kernel
 
